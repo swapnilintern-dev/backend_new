ac2600429ac4c44efb2654ffae34fa9be860226f
MedicaPlus <shivnandan8797048355@gmail.com>
2026-07-04 14:53:42 +0530

    invoice final fix

--- routes/invoiceRoute.js
+++ routes/invoiceRoute.js
@@ -1,6 +1,6 @@
 import express from "express"
 import isAuthenticated from "../middlewares/isAuthenticated.js";
-// import { invoiceGenerate } from "../controller/invoiceController.js";
+import { invoiceGenerate } from "../controller/invoiceController.js";
 
 const router = express.Router() ;
 
